a131071104d598e07d97bc0603c126fe19b35524
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-09-21 22:22:28 +0400

    Add movable object

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
-export { MapObject } from "./map-object";
-export { Obstacle } from "./obstacle";
-export { Position } from "./position";
-export { Bonus } from "./bonus";
+export { MapObject } from './map-object';
+export { Obstacle } from './obstacle';
+export { Position } from './position';
+export { Bonus } from './bonus';
+export { MovableObject } from './movable-object';
---
+++ src/models/movable-object.js
@@ -0,0 +1,22 @@
+import { MapObject } from '.';
+
+function MovableObject(x, y, health, damage, speed) {
+    if (!new.target) {
+        return new MovableObject(x, y, health, damage, speed);
+    }
+
+    MapObject.call(this, x, y, health, damage);
+
+    this.speed = speed;
+
+    this.move = function () {
+        throw Error('Not implemented');
+    };
+
+    // or move this into MapObject
+    this.interact = function () {
+        throw Error('Not implemented');
+    };
+}
+
+export { MovableObject };
