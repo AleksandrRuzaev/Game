d13eca7e72b8f3cd7a98505838353b22b84795dc
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-09-21 23:22:14 +0400

    Add config object

---
+++ src/utils/config.js
@@ -0,0 +1,31 @@
+export const config = {
+    difficult: {
+        easy: {
+            dimensions: {
+                width: 15,
+                height: 10,
+            },
+            monsterCount: 15,
+            bonusCount: 15,
+            obstacleCount: 50,
+        },
+        medium: {
+            dimensions: {
+                width: 30,
+                height: 20,
+            },
+            monsterCount: 50,
+            bonusCount: 40,
+            obstacleCount: 120,
+        },
+        hard: {
+            dimensions: {
+                width: 45,
+                height: 25,
+            },
+            monsterCount: 65,
+            bonusCount: 70,
+            obstacleCount: 300,
+        },
+    },
+};
