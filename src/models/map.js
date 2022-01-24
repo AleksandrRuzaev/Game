7d06352bf6ad5de632b051b651f9c850f67a2cde
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2022-01-24 20:26:20 +0400

    Move map methods to prototype

--- src/models/map.js
+++ src/models/map.js
@@ -5,33 +5,32 @@ function Map(player, monsters, bonuses, obstacles) {
 
     this._player = player;
     this._mapObjects = [player, ...monsters, ...bonuses, ...obstacles];
-
-    this.getMonsters = function () {
-        throw Error('getMonsters not implemented');
-    };
-    this.getObstacles = function () {
-        throw Error('getObstacles not implemented');
-    };
-    this.getBonuses = function () {
-        throw Error('getBonuses not implemented');
-    };
-
-    this.doMove = function () {
-        //through this.getMonsters()
-        throw Error('doMove not implemented');
-    };
-
-    this.interact = function (firstObject, secondObject) {
-        throw Error('interact not implemented');
-    };
-
-    this.exportData = function () {
-        throw Error('exportData not implemented');
-    };
-
-    this.importData = function () {
-        throw Error('importData not implemented');
-    };
 }
+
+Map.prototype.getMonsters = function () {
+    throw Error('getMonsters not implemented');
+};
+Map.prototype.getObstacles = function () {
+    throw Error('getObstacles not implemented');
+};
+Map.prototype.getBonuses = function () {
+    throw Error('getBonuses not implemented');
+};
+Map.prototype.doMove = function () {
+    // through this.getMonsters()
+    throw Error('doMove not implemented');
+};
+Map.prototype.interact = function (firstObject, secondObject) {
+    throw Error('interact not implemented');
+};
+Map.prototype.exportData = function () {
+    throw Error('exportData not implemented');
+};
+Map.prototype.importData = function () {
+    throw Error('importData not implemented');
+};
+Map.prototype.getByPosition = function (position) {
+    throw Error('getByPosition not implemented');
+};
 
 export { Map };
--- src/tests/monster.test.js
+++ src/tests/monster.test.js
@@ -1,98 +1,110 @@
-import { Wolf } from '../models/map-objects/movable-objects/monsters/wolf';
-import { Bear } from '../models/map-objects/movable-objects/monsters/bear';
 import { Player } from '../models/map-objects/movable-objects/player';
 import { Tree } from '../models/map-objects/obstacles/tree';
+import { Map } from '../models/map';
+import { MovableObject } from '../models/map-objects/movable-objects/movable-object';
 
-// 1
-const Wolf = jest.fn();
-const mMock = jest.fn();
-
-Wolf.mockImplementation(() => {
-    return {
-        m: mMock,
-    };
-});
-
-const mock = new Wolf();
-
-// 2
-jest.mock('../models/map-objects/movable-objects/monsters/wolf', () => ({
-    /*here set properties: x: 0, y: 1 ... */
-}));
-
-// 3
-jest.mock('../models/map-objects/movable-objects/monsters/wolf'); // Wolf is now a mock constructor
-Wolf.mockImplementation(() => {
-    return {
-        /*here set properties: x: 0, y: 1 ... */
-    };
-});
-
-const some = new Wolf();
-
-beforeEach(() => {
-    // Clear all instances and calls to constructor and all methods:
-    Wolf.mockClear();
-});
-
-const wolf = new Wolf(0, 0, 10, 2, 2);
-const bear = new Bear(2, 0, 10, 5, 1);
+const wolf = new MovableObject(0, 0, 10, 2, 2);
+const map = new Map();
+const bear = new MovableObject(2, 0, 10, 5, 1);
 const tree = new Tree(2, 0);
 const player = new Player(2, 0, 20, 5, 1);
 
-// before Each clear mock
-// map mock
+jest.mock('../models/map');
+Map.mockImplementation(() => {
+    return {
+        _player: player,
+        _mapObjects: [player, bear, wolf, tree],
+    };
+});
+
+beforeEach(() => {
+    Map.mockClear();
+});
 
 describe('move objects', () => {
-    test('valid monster move', () => {
-        const { x, y } = wolf.position;
-        const direction = 'top';
-
-        wolf.move(direction);
-
-        expect(y).toBe(y + wolf.speed);
-        expect(x).toBe(wolf.position.x);
+    beforeEach(() => {
+        wolf.position = { x: 0, y: 0 };
     });
 
-    test('invalid monster move (collide with another monster)', () => {
-        const { x, y } = wolf.position;
-        const direction = 'right';
+    describe('valid move', () => {
+        test('move top', () => {
+            const { x, y } = wolf.position;
+            const direction = 'top';
 
-        wolf.move(direction);
+            wolf.move(direction);
 
-        expect(y).toBe(wolf.position.y);
-        expect(x).toBe(wolf.position.x);
+            expect(y).toBe(y + wolf.speed);
+            expect(x).toBe(wolf.position.x);
+        });
+
+        test('move bottom', () => {
+            wolf.position = { x: 5, y: 5 };
+
+            const { x, y } = wolf.position;
+            const direction = 'bottom';
+
+            wolf.move(direction);
+
+            expect(y).toBe(y - wolf.speed);
+            expect(x).toBe(wolf.position.x);
+        });
+
+        test('move right', () => {
+            const { x, y } = wolf.position;
+            const direction = 'right';
+
+            wolf.move(direction);
+
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(x + wolf.speed);
+        });
+
+        test('move left', () => {
+            wolf.position = { x: 5, y: 5 };
+
+            const { x, y } = wolf.position;
+            const direction = 'left';
+
+            wolf.move(direction);
+
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(x - wolf.speed);
+        });
     });
 
-    test('invalid monster move (collide with obstacle)', () => {
-        const { x, y } = wolf.position;
-        const direction = 'right';
+    describe('invalid move', () => {
+        test('invalid monster move (collide with another monster)', () => {
+            const { x, y } = wolf.position;
+            const direction = 'right';
+            wolf.move(direction);
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(wolf.position.x);
+        });
 
-        wolf.move(direction);
+        test('invalid monster move (collide with obstacle)', () => {
+            const { x, y } = wolf.position;
+            const direction = 'right';
+            wolf.move(direction);
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(wolf.position.x);
+        });
 
-        expect(y).toBe(wolf.position.y);
-        expect(x).toBe(wolf.position.x);
-    });
+        test('invalid monster move (collide with player)', () => {
+            const { x, y } = wolf.position;
+            const direction = 'right';
+            wolf.move(direction);
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(x + wolf.speed);
+            expect(player.health).toBe(player.health - wolf.damage);
+        });
 
-    test('invalid monster move (collide with player)', () => {
-        const { x, y } = wolf.position;
-        const direction = 'right';
-
-        wolf.move(direction);
-
-        expect(y).toBe(wolf.position.y);
-        expect(x).toBe(x + wolf.speed);
-        expect(player.health).toBe(player.health - wolf.damage);
-    });
-
-    test('invalid monster move (collide with bonus)', () => {
-        const { x, y } = wolf.position;
-        const direction = 'right';
-
-        wolf.move(direction);
-
-        expect(y).toBe(wolf.position.y);
-        expect(x).toBe(x + wolf.speed);
-        // reduce bonus count
+        test('invalid monster move (collide with bonus)', () => {
+            const { x, y } = wolf.position;
+            const direction = 'right';
+            wolf.move(direction);
+            expect(y).toBe(wolf.position.y);
+            expect(x).toBe(x + wolf.speed);
+            // reduce bonus count
+        });
     });
 });
