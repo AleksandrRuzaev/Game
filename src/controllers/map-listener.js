500f6003277bfbd9eccd6eac2dda97966ec084df
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2022-01-24 20:31:45 +0400

    Add listener methods to prototype

--- src/controllers/map-listener.js
+++ src/controllers/map-listener.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-undef */
 function MapListener(map) {
     if (!new.target) {
         return new MapListener(map);
     }
 
-    this.runGame = function () {
-        this._intervalId = setInterval(() => {
-            this.map.doMove();
-        }, 1000);
-    };
-
-    this.endGame = function () {
-        clearInterval(this._intervalId);
-    };
+    this._map = map;
 }
 
+MapListener.prototype.runGame = function () {
+    this._intervalId = setInterval(() => {
+        this._map.doMove();
+    }, 1000);
+};
+MapListener.prototype.endGame = function () {
+    clearInterval(this._intervalId);
+};
 export { MapListener };
