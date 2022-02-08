555e7c39d60463dd399b353afed0b0c680648e81
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2022-02-08 22:47:29 +0400

    Add direction helpers

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -7,3 +7,34 @@ export function instanceOf(instance, instanceConstructor) {
         return proto.constructor === instanceConstructor ? true : instanceOf(proto, instanceConstructor);
     }
 }
+
+export function getRandomDirection() {
+    const direction = Math.floor(Math.random() * 4);
+
+    switch (direction) {
+        case 1:
+            return 'right';
+        case 2:
+            return 'bottom';
+        case 3:
+            return 'left';
+        case 0:
+        default:
+            return 'top';
+    }
+}
+
+export function getPositionByDirection(direction, position, speed) {
+    switch (direction) {
+        case 'top':
+            return { x: position.x, y: position.y + speed };
+        case 'right':
+            return { x: position.x + speed, y: position.y };
+        case 'bottom':
+            return { x: position.x, y: position.y - speed };
+        case 'left':
+            return { x: position.x - speed, y: position.y };
+        default:
+            break;
+    }
+}
