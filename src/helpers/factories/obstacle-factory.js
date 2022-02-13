1e2def8ab0df398925052e092648476ad06664a9
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2022-02-13 23:43:26 +0400

    Add obstacle factory

---
+++ src/helpers/factories/obstacle-factory.js
@@ -0,0 +1,27 @@
+import { Tree } from '../../models/map-objects/obstacles/tree';
+import { Stone } from '../../models/map-objects/obstacles/stone';
+
+export function monsterFactory(type, data) {
+    const x = data?.position.x ?? 0;
+    const y = data?.position.y ?? 0;
+    const wasRemoved = data?.wasRemoved;
+
+    switch (type) {
+        case Tree.name: {
+            const tree = new Tree(x, y);
+
+            tree.wasRemoved = wasRemoved ?? false;
+
+            return tree;
+        }
+        case Stone.name: {
+            const stone = new Stone(x, y);
+
+            stone.wasRemoved = wasRemoved ?? false;
+
+            return stone;
+        }
+        default:
+            break;
+    }
+}
